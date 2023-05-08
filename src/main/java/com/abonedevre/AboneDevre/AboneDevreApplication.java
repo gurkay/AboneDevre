607b1030784c9d6258cc39212c0786e7456fadc6
gurkay <g_basyigit@hotmail.com>
2023-05-09 00:09:03 +0300

    first commit

---
+++ src/main/java/com/abonedevre/AboneDevre/AboneDevreApplication.java
@@ -0,0 +1,17 @@
+package com.abonedevre.AboneDevre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboneDevreApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("AboneDevreApplication");
+
+		SpringApplication.run(AboneDevreApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/abonedevre/AboneDevre/AboneDevreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abonedevre.AboneDevre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboneDevreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
